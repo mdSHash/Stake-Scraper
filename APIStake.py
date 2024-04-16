48bd9bf703ad2d7096de42c0b82aae0fb080c3be
mdSHash <90252510+mdSHash@users.noreply.github.com>
2024-04-17 01:22:49 +0200

    Update APIStake.py

--- APIStake.py
+++ APIStake.py
@@ -1,17 +1,15 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
+from typing import List
 import csv
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Define route to fetch data
-@app.route('/api/data')
-def get_data():
+@app.get('/api/data')
+async def get_data():
     data = []
     # Read data from CSV file
     with open('Tennis_Bets.csv', newline='', encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
         data = list(reader)  # Convert reader to list of dictionaries
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return data
